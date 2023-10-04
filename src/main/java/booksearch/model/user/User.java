7afa36ddcf02e887ff1725a5a3e77507629b7f73
hahahalol9 <cmak7543@mail.ru>
2023-10-04 12:48:40 +0300

    Initial project

---
+++ src/main/java/booksearch/model/user/User.java
@@ -0,0 +1,13 @@
+package booksearch.model.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String
+
+}
